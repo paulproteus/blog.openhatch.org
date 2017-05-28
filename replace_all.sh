2084bf95bb8eb775d555ae2cb3b16d65d4748092
Asheesh Laroia <asheesh@asheesh.org>
2017-05-28 15:45:47 -0700

    Add script to hopefully do all fixups

---
+++ replace_all.sh
@@ -0,0 +1,19 @@
+bash replace_file.sh //openhatch.org/static/packed/openhatch.css?1407021888
+bash replace_file.sh //openhatch.org/static/images/base/landing/feed-icon-16x16-sprited.png
+bash replace_file.sh //openhatch.org/static/images/ship.png
+bash replace_file.sh //openhatch.org/static/css/jquery-ui-lightness/images/pbar-ani.gif
+bash replace_file.sh //openhatch.org/static/images/icons/publish.png
+bash replace_file.sh //openhatch.org/static/images/icons/delete.png
+bash replace_file.sh //openhatch.org/static/images/icons/delete-citation.png
+bash replace_file.sh //openhatch.org/static/images/profile/portfolio/portfolio-entry-bg.png
+bash replace_file.sh //openhatch.org/static/images/wannahelp-button-bg.png
+bash replace_file.sh //openhatch.org/static/images/wannahelp-button-bg-active.png
+bash replace_file.sh //openhatch.org/static/images/wannahelp-button-bg-hover.png
+bash replace_file.sh //openhatch.org/static/images/wannahelp-button-bg-hover-active.png
+bash replace_file.sh //openhatch.org/static/images/bg-translucent-green.png
+bash replace_file.sh //openhatch.org/static/images/dark-hatch.png
+bash replace_file.sh //openhatch.org/static/packed/header.js?1404446267
+bash replace_file.sh //openhatch.org/static/images/the-logo-bluegreen-87px.png
+bash replace_file.sh https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+bash replace_file.sh https://openhatch.org/static/images/the-logo-bluegreen-87px.png
+
--- replace_file.sh
+++ replace_file.sh
@@ -3,9 +3,22 @@ set -eou pipefail
 set -x
 
 pushd downloaded-for-static-snapshot
-wget "$1"
+if echo "$1" | grep -q http ; then
+  URL="$1"
+else
+  URL="https:$1"
+fi
+wget $URL
+FILENAME="$(basename "$URL")"
+if echo "$FILENAME" | grep -q -F '?' ; then
+  NEW_FILENAME="$(echo $FILENAME | sed 's,[?].*,,')"
+  mv "$FILENAME" "$NEW_FILENAME"
+  FILENAME="$NEW_FILENAME"
+fi
+echo $FILENAME
+exit 0
 git add .
 popd
 
-replace "$1" '/downloaded-for-static-snapshot/'"$(basename "$1" )" -- $(git grep -l "$1")
-git commit -a -m 'Replaced file automatically via script: '"$1"
+replace "$URL" '/downloaded-for-static-snapshot/'"$FILENAME" -- $(git grep -l "$1")
+git commit -a -m 'Replaced file automatically via script: '"$URL"
